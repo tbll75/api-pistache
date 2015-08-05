5483e38679e3949cb14e37969cb69e29a41f1fbb
Orazio Locchi <orazio.locchi@gmail.com>
2015-08-05 16:14:34 +0200

    test git

--- api/src/Controller/AccueilController.php
+++ api/src/Controller/AccueilController.php
@@ -6,7 +6,7 @@ class AccueilController{
 
 	public function index(){
 
-		echo "<h1>La vie est belle avec Pistache</h1>";
+		echo "<h1>La vie est belle avec Pistache !</h1>";
 
 		echo "Plus tard il y aura des Menu qui redirigeront vers les methodes appropri&eacute; et ainsi l'interaction Api/Machine pourra se faire depuis un ordi ou un mobile. Mais pas tout de suite hein.";
 
