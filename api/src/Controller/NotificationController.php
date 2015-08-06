6fd094614e2d8653d89de96b30567aa624c48070
Orazio Locchi <orazio.locchi@gmail.com>
2015-08-06 11:12:06 +0200

    NotificationController up -> file acces Recurrent

--- api/src/Controller/NotificationController.php
+++ api/src/Controller/NotificationController.php
@@ -10,22 +10,13 @@ class NotificationController extends SQLController{
 
 	public function recurrent(){
 
-		if(isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {  
-	        $list = explode(',',$_SERVER['HTTP_X_FORWARDED_HOST']);
-	        $_SERVER['HTTP_HOST'] = $list[0];
-	        $_SERVER['SERVER_NAME']=$list[1];
-	  }
-	  echo '<pre>';
-	  print_r($_SERVER);
-	  echo '</pre>';
-/*
 		$momentOfWeek = date('N') - 1;
 		$Hour = date('G');
 
 		echo 'Jour : '.$momentOfWeek."<br/>Heure : ".$Hour."<br/><br/>";
 
 		// traitement horloge
-		if( 8 < $Hour && $Hour < 10){
+		if( 7 < $Hour && $Hour < 9){
 			$momentOfDay = '0';
 		}if( 11 < $Hour && $Hour < 13){
 			$momentOfDay = '1';
@@ -39,14 +30,14 @@ class NotificationController extends SQLController{
 			// Et un p'tit coup de cURL, un !!
 			echo $json = '{"momentOfDay":"'.$momentOfDay.'","momentOfWeek":"'.$momentOfWeek.'"}';
 			$ch = curl_init();
-			curl_setopt( $ch,CURLOPT_URL, $serverPath.'/notification/checkmission' );
+			curl_setopt( $ch,CURLOPT_URL, $_SERVER['SERVER_NAME'].'/notification/checkmission' );
 			curl_setopt( $ch,CURLOPT_POST, true );
 			curl_setopt( $ch,CURLOPT_POSTFIELDS, array('json' => $json) );
 			$result = curl_exec($ch );
 			curl_close( $ch );
 		}else{
 			echo 'momentOfDay n\'est pas definie : Pas de notification lancée pour les tâches recurrentes !<br/>';
-		}*/
+		}
 	}
 
 
