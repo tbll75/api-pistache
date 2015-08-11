2613dcefd6f8d87b58d3d25abaf95d3b3b88f6e9
Orazio Locchi <orazio.locchi@gmail.com>
2015-08-11 17:42:08 +0200

    add merge to mandrill

--- api/src/Controller/MailController.php
+++ api/src/Controller/MailController.php
@@ -59,6 +59,11 @@ class MailController{
 		        "from_name" => "Pistache",
 		        "to" => array(
 		            array(
+		                "email" => "orazio.locchi@gmail.com",
+		                // "name" => "Orazio Locchi",
+		                "type" => "to"
+		            ),
+		            array(
 		                "email" => "orazio.locchi@live.fr",
 		                // "name" => "Orazio Locchi",
 		                "type" => "to"
@@ -66,6 +71,26 @@ class MailController{
 		        ),
 		        "headers" => array(
 		            "Reply-To" => "thibault@pistache-app.com"
+		        ),
+		         "merge_vars" => array(
+		            array(
+		                "rcpt" => "orazio.locchi@gmail.com",
+		                "vars" => array(
+		                    array(
+		                        "name" => "PSWD",
+		                        "content" => "easy le paswd"
+		                    )
+		                )
+		            ),
+		            array(
+		                "rcpt" => "orazio.locchi@live.fr",
+		                "vars" => array(
+		                    array(
+		                        "name" => "PSWD",
+		                        "content" => "tmtc"
+		                    )
+		                )
+		            )
 		        ),
 		        "important" => false,
 		        "track_opens" => null,
@@ -127,44 +152,11 @@ class MailController{
 
 	}
 
-/*	public function welcome(){
+	public function weekly(){
+		// Envoi un mail aux parents avec les stats de l'enfant 
 
-		$mgClient = 'key-e71c0195d23e0930cae6e89f7078f360';
-		$domain = "sandboxffbbdca0be4c417d8af478152afb81d8.mailgun.org";
-		$from = 'postmaster@'.$domain;
-
-		$to = 'Orazio Locchi <orazio.locchi@gmail.com>';
-		$subject = 'Hello Orazio Locchi';
-		$message = 'Congratulations Orazio Locchi, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.';
-
-		$ch = curl_init();
-
-		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-		curl_setopt($ch, CURLOPT_USERPWD, 'api:'.$mgClient);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		$plain = preg_replace("/\<br\s*\/?\>/i", "\n", $message);
-
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-		curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v3/'.$domain.'/messages');
-		curl_setopt($ch, CURLOPT_POSTFIELDS, array('from' => $from,
-			'to' => $to,
-			'subject' => $subject,
-			'html' => $message,
-			'text' => $plain));
-
-		$j = json_decode(curl_exec($ch));
-
-		$info = curl_getinfo($ch);
-
-		if($info['http_code'] != 200)
-		error("Fel 313: Vänligen meddela detta via E-post till ".$from);
-
-		curl_close($ch);
-
-		print_r($j);
 	}
-*/
+
 }
 
  ?>
