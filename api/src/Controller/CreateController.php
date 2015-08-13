6a8245f31b56d09c900d5614151354b011cab806
Orazio Locchi <orazio.locchi@gmail.com>
2015-08-13 11:14:11 +0200

    qsfg

--- api/src/Controller/CreateController.php
+++ api/src/Controller/CreateController.php
@@ -328,10 +328,14 @@ class CreateController extends MailController{
 		foreach ($rep as $choreDone){
 			preg_match('!\d+!', $choreDone['dueDate'], $choreTimeStamp);
 			$choreTimeStamp =  substr($choreTimeStamp[0], 0, 10);
-			echo "<br/>".$choreDone['dueDate']."<br/>".intval($choreDone['dueDate'])."<br/>".$choreTimeStamp;
 			$recurrentDone[] = array("idChild" => $choreDone['Children_idChildren'], "idChoreRec" => $choreDone['ChoreRec_idChoreRec'], "today" => $today, "day" => $choreDone['momentOfWeek'], "moment" => $choreDone['momentOfDay']); // 4 -> toute la journée
 
 		}
+
+		echo '<pre>';
+		print_r($recurrentDone);
+		echo '</pre>';
+
 
 	}
 
