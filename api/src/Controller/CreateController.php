03c35063ec5cbacdb00add5951d4722a89e3db10
Orazio Locchi <orazio.locchi@gmail.com>
2015-08-13 11:00:02 +0200

    yesterday

--- api/src/Controller/CreateController.php
+++ api/src/Controller/CreateController.php
@@ -280,7 +280,8 @@ class CreateController extends MailController{
 		// today		
 		$momentOfWeek = date('N') - 1;
 		$jour = array("0" => "lundi", "1" => "mardi", "2" => "mercredi", "3" => "jeudi", "5" => "vendredi", "5" => "samedi", "6" => "dimanche");
-		$today = date('d-m-Y');
+		$today = strtotime(date('d-m-Y'));
+		$yesterday = $today - 60*60*24;
 
 		// select recurrent REC
 		$rep = $this->select("SELECT * FROM api_ChoreRec WHERE isRecurrent = 1 AND $jour[$momentOfWeek] = 1 AND isActive = 1");
