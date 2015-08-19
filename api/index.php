e38e7f68244e21ff5feee9abae727cedb208b0a7
Orazio Locchi <orazio.locchi@gmail.com>
2015-08-19 19:51:40 +0200

    gqf

--- api/index.php
+++ api/index.php
@@ -46,83 +46,17 @@
 // (Je vais mettre les procédure comme elles viennent en ouverture de jeu)
 
 // INSERT
-	$router->post('/create/family', 'Create#family');//->with('name', '[a-zA-Z]+')->with('mail', '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'); // rentre dans la bdd (si mail non existant) et retourne id.
-	$router->post('/create/child', 'Create#child');
-	$router->post('/create/parent', 'Create#parent');
-	$router->post('/create/settings', 'Create#settings');
-	$router->post('/create/chore', 'Create#chore');
-	$router->post('/create/choredone', 'Create#choredone');
-	$router->post('/create/unlockobject', 'Create#unlockobject');
-	$router->post('/create/support', 'Create#support');
-
 	$router->post('/create', 'Create#dispatch');
 
 // UPDATE
-	$router->post('/update/family', 'Update#family'); 
-	$router->post('/update/child', 'Update#child'); 
-	$router->post('/update/parent', 'Update#parent');
-	$router->post('/update/settings', 'Update#settings');
-	$router->post('/update/chore', 'Update#chore');
-	$router->post('/update/choredone', 'Update#choredone');
-	$router->post('/update/unlockobject', 'Update#unlockobject');
-	$router->post('/update/alldata', 'Update#alldata');
-	$router->post('/update/password', 'Update#pass');
-
 	$router->post('/update', 'Update#dispatch');
-
 
 // DELETE
 	$router->post('/delete', 'Delete#entity');
 
-
-
 // GETTERS
 	$router->post('/get', 'Get#dispatch');
-// Getters Family ********************************************************
-	$router->get('/family/:idFamily/member', "Family#member")->with('idFamily', '[0-9]+');
-	$router->get('/family/:idFamily/parent', "Family#parent")->with('idFamily', '[0-9]+');
-	$router->get('/family/:idFamily/children', "Family#children")->with('idFamily', '[0-9]+');
-	$router->get('/family/:id/:champs', "Family#showfields")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/family/:id', "Family#show")->with('id', '[0-9]+');
-	$router->get('/family/:champs', "Family#indexfields")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/family', "Family#index");
-// LE getters par excellence. Il donne tout.
-	$router->get('/family/alldata/:id', 'Family#alldata')->with('id', '[0-9]+'); // donne un idFamily, et recois touutes les infos de cette familles (enfant hero settings parents etc..)
 
-// Getters children ********************************************************
-	$router->get('/children/:id/:champs', "Children#showfields")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/children/:id', "Children#show")->with('id', '[0-9]+');
-	$router->get('/children/:champs', "Children#indexfields")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/children', "Children#index");
-
-// Getters parent ********************************************************
-	$router->get('/parent/:id/:champs', "Parent#showfields")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/parent/:id', "Parent#show")->with('id', '[0-9]+');
-	$router->get('/parent/:champs', "Parent#indexfields")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/parent', "Parent#index");
-
-// Getters hero ********************************************************
-	$router->get('/hero/:id/:champs', "Hero#showfields")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/hero/:id', "Hero#show")->with('id', '[0-9]+');
-	$router->get('/hero/:champs', "Hero#indexfields")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/hero', "Hero#index");
-
-// Getters Chore ********************************************************
-	$router->get('/chore/:id/:champs', "Chore#showfields")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/chore/:id', "Chore#show")->with('id', '[0-9]+');
-	$router->get('/chore/:champs', "Chore#indexfields")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/chore', "Chore#index");
-
-	$router->get('/choredone/:id/:champs', "Chore#showfieldsDone")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/choredone/:id', "Chore#showDone")->with('id', '[0-9]+');
-	$router->get('/choredone/:champs', "Chore#indexfieldsDone")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/choredone', "Chore#indexDone");
-
-// Getters Settings ********************************************************
-	$router->get('/settings/:id/:champs', "Settings#showfields")->with('id', '[0-9]+')->with('champs', '[a-z\-0-9,]+');
-	$router->get('/settings/:id', "Settings#show")->with('id', '[0-9]+');
-	$router->get('/settings/:champs', "Settings#indexfields")->with('champs', '[a-z\-0-9,]+');
-	$router->get('/settings', "Settings#index");
 
 //  Getters Partenaire
 	$router->get('/partner/familychildren/', "Partner#familychildren");
